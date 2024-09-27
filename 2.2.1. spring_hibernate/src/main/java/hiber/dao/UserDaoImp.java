277de269c06c232b65b114285787e0808bb4ff2d
Geregorio <gpnitsevich@mail.ru>
2024-09-27 15:48:17 +0300

    Check 2

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -30,13 +30,10 @@ public class UserDaoImp implements UserDao {
    @Override
    @SuppressWarnings("unchecked")
    public List<User> userByCar(String model, int series) {
-//      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User where car.model = 'Porsche' and car.series = 911");
       String hql = "from User where car.model = :model and car.series = :series";
       Query<User> query = sessionFactory.getCurrentSession().createQuery(hql,User.class);
       query.setParameter("model", model);
       query.setParameter("series", series);
-      //query.executeUpdate();
       return query.getResultList();
    }
-
 }
